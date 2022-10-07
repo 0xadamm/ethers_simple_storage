5b42251bb471a999bff78a37cc1dfaebb33839c3
0xadamm <adam.mourade@gmail.com>
2022-10-07 00:43:32 -0700

    deployed contracts using ethers.js

--- deploy.ts
+++ deploy.ts
@@ -1,19 +1,28 @@
 import { ethers } from "ethers";
 import * as fs from "fs-extra";
+import "dotenv/config";
+
 // RPC: http://127.0.0.1:7545
 
 const main = async () => {
+  // console.log(process.env.PRIVATE_KEY);
   // Connect to the RPC Probider to ethers
-  const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:7545"
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+
+  // Create signer | wallet  variable
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+
+  // Create a variable that is the encrypted JSON
+  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8");
+  console.log(encryptedJson);
+
+  // Create new encrypted wallet by passing in JSON encyrption
+  let encryptedWallet = await ethers.Wallet.fromEncryptedJson(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PASSWORD!
   );
 
-  // Create signer variable
-  const wallet = new ethers.Wallet(
-    // Never use private key in real project !!
-    "008eb98165004a77be815d101d0033e374af7ebfbde6d7f3ecab8df522ca68d2",
-    provider
-  );
+  console.log(encryptedWallet);
 
   // Create abi | interface variable
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
@@ -24,10 +33,40 @@ const main = async () => {
     "utf8"
   );
 
-  // Pass in (interface, bytecode, signer)
-  //Deploys new contract
-  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  console.log("Deploying, please wait");
+  // Connect (interface, bytecode, signer)
+  const contractFactory = new ethers.ContractFactory(
+    abi,
+    binary,
+    encryptedWallet
+  );
+  console.log("Deploying, please wait....");
+  // Deploys Contract
   const contract = await contractFactory.deploy();
-  console.log(contract);
+
+  // Waits for contract to deploy then creates deployment response
+  const deploymentReceipt = await contract.deployTransaction.wait(1);
+  // console.log("Deployment Transaction...");
+  // console.log(contract.deployTransaction); // Deployment Response
+  // console.log("Transaction Receipt...");
+  // console.log(deploymentReceipt); // Receipt is from next block
+
+  // Get favorite number from contract
+  const currentFavoriteNumber = await contract.retrieve();
+  console.log(`Currnt favorite number: ${currentFavoriteNumber.toString()}`);
+
+  // Call the store function on the contract and store a new favorite number
+  const transctionResponse = await contract.store("8");
+
+  const transctionReceipt = await transctionResponse.wait(1);
+
+  // Get new favorite number from contract
+  const updatedFavoriteNumber = await contract.retrieve();
+  console.log(`Updated favorite number is: ${updatedFavoriteNumber}`);
 };
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
